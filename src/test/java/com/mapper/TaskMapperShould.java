df2d61929e0601f78b56cace4fa05b7685981583
Agamemnon Theodorakopoulos <memos.t96@gmail.com>
2019-12-04 18:13:52 +0200

    Add tests In TaskMapperShould

--- src/test/java/com/mapper/TaskMapperShould.java
+++ src/test/java/com/mapper/TaskMapperShould.java
@@ -1,6 +1,7 @@
 package com.mapper;
 
 import com.unisystems.enums.EmployeeStatusEnum;
+import com.unisystems.enums.TaskDifficultyEnum;
 import com.unisystems.enums.TaskStatusEnum;
 import com.unisystems.mapper.TaskMapper;
 import com.unisystems.model.Department;
@@ -13,15 +14,19 @@ import com.unisystems.response.TaskResponse;
 import com.unisystems.response.generic.Error;
 import com.unisystems.response.generic.GenericResponse;
 import com.unisystems.response.getAllResponse.GetTaskByIdResponse;
+import com.unisystems.strategy.taskStrategy.SearchDifficultyStrategy;
+import com.unisystems.strategy.taskStrategy.SearchDifficultyStrategyFactory;
 import com.unisystems.utils.Utils;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.when;
@@ -51,6 +56,15 @@ public class TaskMapperShould {
 
     @InjectMocks
     Utils utils;
+
+    @Mock
+    SearchDifficultyStrategyFactory searchDifficultyStrategyFactory;
+
+    @Mock
+    List<String> assignedEmployees;
+
+    @Mock
+    List<String> updates;
 
 
     @Before
@@ -135,10 +149,70 @@ public class TaskMapperShould {
         when(mockedTaskMapper.getGenericResponseById("1",tasks)).thenReturn(responseData);
         when(mockedTaskMapper.getGenericResponseById("k",tasks)).thenReturn(responseError);
 
+
         GenericResponse<GetTaskByIdResponse> actualDto = mockedTaskMapper.getGenericResponseById("1",tasks);
         GenericResponse<GetTaskByIdResponse> actualNumericError = mockedTaskMapper.getGenericResponseById("k",tasks);
         assertNotNull(actualDto);
         Assert.assertEquals(task.getTitle(),actualDto.getData().getTaskResponses().get(0).getTaskTitle());
         Assert.assertEquals("ID NUMERIC ONLY",actualNumericError.getErrors().get(0).getErrorMessage());
    }
+
+   @Test
+   @Ignore
+    public void returnTaskByDifficulty(){
+        List<TaskByIdResponse> tasks = (List<TaskByIdResponse>) new ArrayList<TaskByIdResponse>();
+       List<Error> errors = new ArrayList<Error>();
+       TaskByIdResponse Task = new TaskByIdResponse(1,"Title","TaskDEsc", TaskDifficultyEnum.EASY,TaskStatusEnum.DONE,assignedEmployees,updates);
+
+       tasks.add(Task);
+       List<TaskByIdResponse> taskResponse = new ArrayList<>();
+       GenericResponse<GetTaskByIdResponse> responseData = new GenericResponse<>();
+       GenericResponse<GetTaskByIdResponse> responseError = new GenericResponse<>();
+       taskResponse.add((Task));
+       responseData.setData( new GetTaskByIdResponse(taskResponse));
+       Error numericError = new Error(1023, "SearchCriteria incorrect", "It should be one of the following: [easy,medium,hard]");
+       errors.add(numericError);
+       responseError.setErrors(errors);
+
+       when(mockedTaskMapper.getTasksWithDifficulty("EASY",tasks)).thenReturn(responseData);
+       when(mockedTaskMapper.getTasksWithDifficulty("New",tasks)).thenReturn(responseError);
+       when(searchDifficultyStrategyFactory.makeStrategyForDifficulty("EASY")).thenReturn((SearchDifficultyStrategy) responseData);
+
+
+       GenericResponse<GetTaskByIdResponse> actualDto = mockedTaskMapper.getTasksWithDifficulty("EASY",tasks);
+       GenericResponse<GetTaskByIdResponse> actualNumericError = mockedTaskMapper.getTasksWithDifficulty("New",tasks);
+       assertNotNull(actualDto);
+       Assert.assertEquals(Task.getTaskTitle(),actualDto.getData().getTaskResponses().get(0).getTaskTitle());
+       Assert.assertEquals("SearchCriteria incorrect",actualNumericError.getErrors().get(0).getErrorMessage());
+   }
+
+   @Test
+   public void returnTaskByEmployee(){
+       List<Task> tasks = (List<Task>) taskRepository.findAll();
+       List<Error> errors = new ArrayList<Error>();
+       List<TaskByIdResponse> taskResponse = new ArrayList<TaskByIdResponse>();
+       GenericResponse<GetTaskByIdResponse> responseData = new GenericResponse<>();
+       GenericResponse<GetTaskByIdResponse> responseError = new GenericResponse<>();
+       Task task = new Task("New Portal","This is a task for making a new portal",5,9,8, TaskStatusEnum.NEW);
+       task.setId(1L);
+       taskResponse.addAll(taskMapper.mapAllTasksById(tasks,"1"));
+       responseData.setData( new GetTaskByIdResponse(taskResponse));
+       Error numericError = new Error(25, "Wrong assignedEmployees parameter",
+               "The provided assignedEmployees parameter should be numeric");
+       errors.add(numericError);
+       responseError.setErrors(errors);
+       when(mockedTaskMapper.findByAssignedEmployees("1",tasks)).thenReturn(responseData);
+       when(mockedTaskMapper.findByAssignedEmployees("k",tasks)).thenReturn(responseError);
+
+
+       GenericResponse<GetTaskByIdResponse> actualDto = mockedTaskMapper.findByAssignedEmployees("1",tasks);
+       GenericResponse<GetTaskByIdResponse> actualNumericError = mockedTaskMapper.findByAssignedEmployees("k",tasks);
+       assertNotNull(actualDto);
+       Assert.assertEquals(task.getTitle(),actualDto.getData().getTaskResponses().get(0).getTaskTitle());
+       Assert.assertEquals("Wrong assignedEmployees parameter",actualNumericError.getErrors().get(0).getErrorMessage());
+
+   }
+
+
+
 }
